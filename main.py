fb52ad4c0b3314d052a95bdbe3f94539a90321d4
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2024-01-20 17:48:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from langchain.prompts import PromptTemplate
 from typing import Final
 from telegram import Update
+from telegram.constants import ChatAction
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 import os
 
